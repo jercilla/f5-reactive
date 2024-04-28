e76a7ccb25546d047f8e4fcade025bc17aae1008
jercilla <joseba.ercilla@gmail.com>
2024-04-28 23:04:07 +0200

    first commit

---
+++ reactive.js
@@ -0,0 +1,50 @@
+class Observable {
+
+  constructor(value) {
+    this._listeners = [];
+    this._value = value;
+  }
+
+  notify() {
+    this._listeners.forEach(listener => listener(this._value));
+  }
+
+  subscribe(listener) {
+    this._listeners.push(listener);
+  }
+
+  get value() {
+    return this._value;
+  }
+
+  set value(val) {
+    if (val !== this._value) {
+      this._value = val;
+      this.notify();
+    }
+  }
+}
+
+const bindData = (data) => {
+  Object.keys(data).forEach(key => {
+    const input = document.getElementById(key);
+    const observable = data[key];
+    
+    input.value = observable.value;
+    observable.subscribe(() => input.value = observable.value);
+    input.oninput = () => observable.value = input.valueAsNumber;
+    const debug = document.getElementById('debug');
+    if (debug) {
+      debugData(data);
+      observable.subscribe(() => debugData(data));
+    }
+  })
+}
+
+const debugData = (data) => {
+  let debug_data = "";
+        Object.keys(data).forEach(key => {
+          debug_data += key + ": " + data[key].value + "\n";
+        })
+        debug.innerHTML = debug_data;      
+}
